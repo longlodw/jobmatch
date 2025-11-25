c9004fe758e979ecdd39c6c50df18d4a99c7b8e8
longlodw <longlodw@gmail.com>
2025-11-24 22:52:13 -0500

    put apikey in header instead of query for better security

--- fetcher.go
+++ fetcher.go
@@ -5,8 +5,9 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
-	"net/url"
 	"strings"
+
+	"go.uber.org/zap"
 )
 
 type IJobFetcher interface {
@@ -21,12 +22,14 @@ type IdMarshaledJob struct {
 type JobFetcher struct {
 	urlStr string
 	token  string
+	logger *zap.Logger
 }
 
-func NewJobFetcher(urlStr, token string) *JobFetcher {
+func NewJobFetcher(urlStr, token string, logger *zap.Logger) *JobFetcher {
 	return &JobFetcher{
 		urlStr: urlStr,
 		token:  token,
+		logger: logger,
 	}
 }
 
@@ -38,11 +41,13 @@ func (jf *JobFetcher) Fetch(ctx context.Context, searchUrl string) ([]IdMarshale
 	}
 	bodyJson, err := json.Marshal(bodyObj)
 	if err != nil {
+		jf.logger.Error("Failed to marshal fetch body", zap.String("searchUrl", searchUrl), zap.Error(err))
 		return nil, err
 	}
 	bodyReader := strings.NewReader(string(bodyJson))
 	resp, err := fetchUrl(ctx, jf.urlStr, jf.token, bodyReader)
 	if err != nil {
+		jf.logger.Error("Failed to fetch jobs", zap.String("searchUrl", searchUrl), zap.Error(err))
 		return nil, err
 	}
 	defer resp.Body.Close()
@@ -50,25 +55,20 @@ func (jf *JobFetcher) Fetch(ctx context.Context, searchUrl string) ([]IdMarshale
 	decoder := json.NewDecoder(resp.Body)
 	err = decoder.Decode(&jobs)
 	if err != nil {
+		jf.logger.Error("Failed to decode jobs response", zap.String("searchUrl", searchUrl), zap.Error(err))
 		return nil, err
 	}
 	return jobs, nil
 }
 
 func fetchUrl(ctx context.Context, urlStr, token string, body io.Reader) (*http.Response, error) {
-	parsedURL, err := url.Parse(urlStr)
-	if err != nil {
-		return nil, err
-	}
-	query := parsedURL.Query()
-	query.Set("token", token)
-	parsedURL.RawQuery = query.Encode()
-	newUrl := parsedURL.String()
 	client := &http.Client{}
-	req, err := http.NewRequest("POST", newUrl, body)
+	req, err := http.NewRequest("POST", urlStr, body)
 	if err != nil {
 		return nil, err
 	}
 	req.WithContext(ctx)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+token)
 	return client.Do(req)
 }
